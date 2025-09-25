3b4bf5c3645114cd407b8d0573d2c745b2ad4834
Tyler Ekizian <TylerEkizian@yahoo.com>
2025-09-25 03:07:58 -0400

    Checking jwks manually

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ import "dotenv/config";
 import express from "express";
 import cors from "cors";
 import { auth } from "express-oauth2-jwt-bearer";
+import https from "node:https";
 
 import todo from "./src/todo/index.js";
 import pool from "./src/utils/db/index.js";
@@ -10,14 +11,38 @@ const app = express();
 const PORT = process.env.PORT || 5000;
 const { AUTH0_AUDIENCE, AUTH0_ISSUER_BASE_URL, ALLOWED_HOSTS } = process.env;
 
+const jwksUrl = `${AUTH0_ISSUER_BASE_URL}.well-known/jwks.json`;
+
 let jwtCheck;
 try {
+  console.log(`Checking connectivity to Auth0 JWKS endpoint: ${jwksUrl}`);
+  await new Promise((resolve, reject) => {
+    https
+      .get(jwksUrl, (res) => {
+        if (res.statusCode === 200) {
+          console.log("Successfully connected to Auth0 JWKS endpoint.");
+          resolve(res);
+        } else {
+          console.error(
+            `Failed to connect to Auth0 JWKS endpoint. Status code: ${res.statusCode}`
+          );
+          console.error("This may be the source of the InvalidRequestError.");
+          // Consider adding code to gracefully exit or log more details
+          reject(res.statusCode);
+        }
+      })
+      .on("error", (e) => {
+        console.error(
+          `Error during Auth0 JWKS connectivity test: ${e.message}`
+        );
+        reject(e);
+      });
+  });
   console.log("Attempting Auth0 middleware initialization");
   jwtCheck = auth({
     audience: AUTH0_AUDIENCE,
     issuerBaseURL: AUTH0_ISSUER_BASE_URL,
-    tokenSigningAlg: "RS256",
-    jwksUri: `${AUTH0_ISSUER_BASE_URL}.well-known/jwks.json`,
+    jwksUri: jwksUrl,
   });
   console.log("Auth0 middleware initialized successfully");
 } catch (err) {
